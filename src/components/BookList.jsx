d1326e4b7507ac7532855703d80a9b2a08a92499
Abhishek Kumar Sharma <abhishek.sharma@byteme.co.in>
2023-04-11 22:53:45 +0530

    changes

---
+++ src/components/BookList.jsx
@@ -0,0 +1,23 @@
+import React, { useContext } from "react";
+import BooksContext from "../context/books";
+import BookShow from "./BookShow";
+
+function BookList() {
+   const { books } = useContext(BooksContext);
+
+   const renderedBooks = books.map((book) => {
+      return (
+         <BookShow
+            key={book.id}
+            book={book}
+         />
+      );
+   });
+   return (
+      <div className="book-list">
+         {renderedBooks}
+      </div>
+   )
+}
+
+export default BookList;
